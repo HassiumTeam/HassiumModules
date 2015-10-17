e536df9e7b7e1354cfcc88dd9644475b7bd538e5
zdimension <zippedfire@free.fr>
2015-10-18 00:22:32 +0200

    Added Math Library © zdimension

---
+++ IMPressive/IMPressive/Constructors.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Hassium.Functions;
+using Hassium.HassiumObjects;
+using Hassium.Interpreter;
+using IMPression;
+
+namespace IMPressive
+{
+    public class Constructors : ILibrary
+    {
+        public static Dictionary<string, HassiumObject> GetConstants()
+        {
+            var res = new Dictionary<string, HassiumObject>();
+
+            res.Add("I", new HComplex(0, 1));
+
+            #region Constants
+            var cts = new HassiumObject();
+            cts.Attributes.Add("Pi", new InternalFunction(x => (HComplex)Constants.Pi, 0, true));
+            cts.Attributes.Add("E", new InternalFunction(x => (HComplex)Constants.E, 0, true));
+            cts.Attributes.Add("Catalan", new InternalFunction(x => (HComplex)Constants.Catalan, 0, true));
+            cts.Attributes.Add("Degree", new InternalFunction(x => (HComplex)Constants.Degree, 0, true));
+            cts.Attributes.Add("Epsilon", new InternalFunction(x => (HComplex)Constants.Epsilon, 0, true));
+            cts.Attributes.Add("EulerGamma", new InternalFunction(x => (HComplex)Constants.EulerGamma, 0, true));
+            cts.Attributes.Add("Glaisher", new InternalFunction(x => (HComplex)Constants.Glaisher, 0, true));
+            cts.Attributes.Add("Khinchin", new InternalFunction(x => (HComplex)Constants.Khinchin, 0, true));
+            cts.Attributes.Add("Phi", new InternalFunction(x => (HComplex)Constants.Phi, 0, true));
+            cts.Attributes.Add("SpeedOfLight", new InternalFunction(x => (HComplex)Constants.SpeedOfLight, 0, true));
+            cts.Attributes.Add("Tau", new InternalFunction(x => (HComplex)Constants.Tau, 0, true));
+
+            res.Add("Constants", cts);
+            #endregion
+
+            #region Functions
+            var imp = new HassiumObject();
+
+            var funcs =
+                typeof(Functions).GetMethods(BindingFlags.Public |
+                                                  BindingFlags.Static);
+            var ln = "";
+            try
+            {
+                foreach (var x in funcs)
+                {
+                    if (Attribute.IsDefined(x, typeof(MathFunc)))
+                        if ((Attribute.GetCustomAttribute(x, typeof(MathFunc)) as MathFunc).Hide) continue;
+                    var n = x.Name;
+                    ln = n;
+                    if (Attribute.IsDefined(x, typeof(MathFunc)))
+                        if ((Attribute.GetCustomAttribute(x, typeof(MathFunc)) as MathFunc).Name != "")
+                            n = (Attribute.GetCustomAttribute(x, typeof(MathFunc)) as MathFunc).Name;
+
+                    if (imp.Attributes.Any(f => f.Key == n)) continue;
+
+                    if(funcs.Any(f => f.Name == n && f.GetParameters().Length != x.GetParameters().Length))
+                    {
+                        var overloads =
+                            funcs.Where(f => f.Name == n && f.GetParameters().Length != x.GetParameters().Length);
+                        var parameters = overloads.Select(f => f.GetParameters().Length).ToList();
+                        parameters.Insert(0, x.GetParameters().Length);
+
+
+                        imp.Attributes.Add(n, new InternalFunction(a =>
+                        {
+                            return new HComplex((Complex)overloads.First(f => f.GetParameters().Length == a.Length)
+                                .Invoke(null, a.Select(y => (object) y.HComplex().Value).ToArray()));
+                        }, parameters.ToArray()));
+                    }
+                    else imp.Attributes.Add(n, new InternalFunction(a => new HComplex((Complex)x.Invoke(null, a.Select(y => (object)y.HComplex().Value).ToArray())), x.GetParameters().Length));
+                }
+            }
+            catch
+            {
+                throw new ParseException("Error while loading function: " + ln, -1);
+            }
+
+
+            imp.Attributes.Add("integrate",
+                new InternalFunction(
+                    x =>
+                        new HComplex(Functions.Integrate(
+                            y => HassiumMethod.GetFunc1(x[0])(new HComplex(y)).HComplex().Value, x[1].HComplex().Value,
+                            x[2].HComplex().Value)), 3));
+
+
+            res.Add("IMP", imp);
+            #endregion
+
+            return res;
+        }
+
+        [IntFunc("Complex", true, new []{0, 1, 2})]
+        public static HassiumObject Complex(HassiumObject[] args)
+        {
+            if(args.Length == 0) return new HComplex(0, 0);
+            if(args.Length == 1) return new HComplex(args[0].HDouble().Value, 0);
+            else return new HComplex(args[0].HDouble().Value, args[1].HDouble().Value);
+        }
+    }
+}
---
+++ IMPressive/IMPressive/HComplex.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hassium.Functions;
+using Hassium.HassiumObjects;
+using Hassium.HassiumObjects.Types;
+using Hassium.Interpreter;
+using IMPression;
+
+namespace IMPressive
+{
+    public class HComplex : HassiumObject
+    {
+        public double Real
+        {
+            get { return Value.Real; }
+            set { Value = new Complex(value, Imaginary); }
+        }
+
+        public double Imaginary
+        {
+            get { return Value.Imaginary; }
+            set { Value = new Complex(Real, value); }
+        }
+
+        public HComplex(Complex c) : this(c.Real, c.Imaginary)
+        {
+
+        }
+
+        public HComplex(double r, double i) : this()
+        {
+            Value = new Complex(r, i);
+        }
+
+        public HComplex()
+        {
+            Value = new Complex(0, 0);
+
+            Attributes.Add("real", new HassiumProperty("real", x => Real, (self, x) => Real = x[0].HDouble().Value));
+            Attributes.Add("imaginary", new HassiumProperty("imaginary", x => Imaginary, (self, x) => Imaginary = x[0].HDouble().Value));
+            Attributes.Add("imag", new HassiumProperty("imag", x => Imaginary, (self, x) => Imaginary = x[0].HDouble().Value));
+
+            Attributes.Add("module", new HassiumProperty("module", x => (double)Value.Module, null, true));
+            Attributes.Add("argument", new HassiumProperty("argument", x => (double)Value.Argument, null, true));
+            Attributes.Add("conjugate", new HassiumProperty("conjugate", x => (double)Value.Conjugate, null, true));
+
+            Attributes.Add("__add", new InternalFunction(Add, -1));
+            Attributes.Add("__substract", new InternalFunction(Substract, -1));
+            Attributes.Add("__multiply", new InternalFunction(Multiply, -1));
+            Attributes.Add("__divide", new InternalFunction(Divide, -1));
+
+            Attributes.Add("__compare", new InternalFunction(x =>
+            {
+                var v = x[0].HComplex().Value;
+                if (v > Value) return 1;
+                if (v < Value) return -1;
+                return 0;
+            }, 1));
+
+            Attributes.Add("__pow", new InternalFunction(x => new HComplex(Functions.Pow(Value, x[0].HComplex().Value)), 1));
+            Attributes.Add("__root", new InternalFunction(x => new HComplex(Functions.NthRoot(Value, x[0].HComplex().Value)), 1));
+
+            Attributes.Add("negate", new InternalFunction(Negate, 0));
+
+            Attributes.Add("square", new InternalFunction(x => new HComplex(Value.Square()), 0));
+        }
+
+        public HassiumObject Negate(HassiumObject[] args)
+        {
+            return new HComplex(-Value);
+        }
+
+        public HassiumObject Add(HassiumObject[] args)
+        {
+            return new HComplex(args.Select(x => x.HComplex().Value).Sum() + Value);
+        }
+
+        public HassiumObject Substract(HassiumObject[] args)
+        {
+            return new HComplex(Value - args.Select(x => x.HComplex().Value).Sum());
+        }
+
+        public HassiumObject Multiply(HassiumObject[] args)
+        {
+            return new HComplex(Value * args.Select(x => x.HComplex().Value).Aggregate(Complex.One, (x, y) => x * y));
+        }
+
+        public HassiumObject Divide(HassiumObject[] args)
+        {
+            return new HComplex(Value * args.Select(x => 1 / x.HComplex().Value).Aggregate(Complex.One, (x, y) => x * y));
+        }
+
+        public Complex Value { get; private set; }
+
+        public static implicit operator HComplex(HassiumDouble d)
+        {
+            return new HComplex(d.Value, 0);
+        }
+
+        public static implicit operator HComplex(Complex c)
+        {
+            return new HComplex(c.Real, c.Imaginary);
+        }
+
+        public static explicit operator Complex(HComplex c)
+        {
+            return new Complex(c.Real, c.Imaginary);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static HComplex HComplex(this HassiumObject obj)
+        {
+            if (obj is HassiumDouble || obj is HassiumInt) return obj.HDouble();
+            if (obj is HComplex) return (HComplex) obj;
+            throw new ParseException("Only double and int can be cast to Complex", -1);
+        }
+    }
+}
